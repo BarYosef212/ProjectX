f1e3cb3db19fcb678f24e538eb6ea929fbb2803a
Bar Yosef Tayar Mac <bartayar123456789@gmail.com>
2024-08-29 17:00:47 +0300

    adding header to pages using ejs

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require ('express'); // Import library express (npm install express)
 const path = require ('path'); // Import library path (access to file system)
-const shoesRoutes = require ('./routes/shoesRoutes'); // Import file shoesStore.js
 
 const mongoose = require ('mongoose'); // Import library mongoose (connect to MongoDB) (npm install mongoose)
 const app = express(); // Create an express application
 const PORT = 3000; // Port number
 
 app.set('view engine', 'ejs'); // Set view engine to ejs  (npm install ejs)
-app.set('Pages', path.join(__dirname, 'Pages')); // Set Pages to the Pages folder
+app.set('views', path.join(__dirname, 'Pages')); // Set Pages to the Pages folder
 
 async function connectDB(){ // Connect to MongoDB
   try{
-    await mongoose.connect('mongodb://localhost:27017/shoesDB', {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect('mongodb://localhost:27017/shoesDB');
     console.log('Connected to MongoDB'); // If connected to MongoDB, print this message
   }
   catch(error){
@@ -22,8 +21,11 @@ async function connectDB(){ // Connect to MongoDB
 connectDB(); // Call the function connectDB
 
 app.use(express.static(path.join(__dirname, 'public'))); // Use the public folder
-
-app.use(shoesRoutes)// Use the shoesRoutes
+// app.use(homeRoute);
+app.get('/', (req, res) => { // Get the orders
+  res.render('Sign-up'); // Render the home page // cahnge here///
+});
+// app.use(shoesRoutes)// Use the shoesRoutes
 
 app.listen(PORT, () => { // Listen to the port
   console.log(`Server is running on http://localhost:${PORT}`); // If the server is running, print this message
