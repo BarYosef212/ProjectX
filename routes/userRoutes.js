f42c7cbfc4011c316c130427c02329cd20c10ecc
Tomer Porat <Tomer@Gadis-MacBook-Pro-2.local>
2024-09-25 12:35:16 +0300

    all Branches Metohud

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 const Branch = require('../models/Branch');
-
-
+const branchController = require('../controllers/branch'); // Controller for branch logic
+const branchServices = require('../services/branch'); // Service layer for branch logic
 
 router.get('/',userController.renderHomePage)
 router.get('/home',userController.renderHomePage)
@@ -38,20 +38,6 @@ router.get('/UpdateBranch', (req,res)=>{
 })
 router.post('/UpdateBranch', branchController.updateBranch);
 
-router.get('/BranchList', async (req, res) => {
-  try {
-    const branchName = req.query.branchName; // Get the branch name from the query parameters
-        console.log("Searching for branch:", branchName);
-        const branches = await Branch.find({ Store_Name: branchName });
+router.get('/BranchList', branchController.getBranchList);
 
-        if (branches.length === 0) {
-            res.render('branches', { error: 'No branches found.', branches: [] });
-        } else {
-            res.render('branches', { branches }); // Render the page with the found branches
-        }
-    } catch (error) {
-        console.error("Error fetching branches:", error);
-        res.render('branches', { error: error.message, branches: [] });
-    }
-});
-module.exports = router
+module.exports = router;
